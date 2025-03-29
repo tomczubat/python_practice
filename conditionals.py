27f3b1771a88ed8b67eb803df042abba26c32b28
tczubat@gmail.com <tczubat@gmail.com>
2025-03-29 10:13:54 -0400

    adding conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+
+x = 5
+y = 1
+
+if x > y:
+    print(str(x) + " is greater than " + str(y))
+elif x == y:
+    print(str(x) + " is equal to " + str(y))
+else:
+    print(str(x) + " is less than " + str(y))
