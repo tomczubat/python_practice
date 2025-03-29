84058f2407362d1f2b68a3305ef7d7fb4001e59d
tczubat@gmail.com <tczubat@gmail.com>
2025-03-29 15:23:48 -0400

    updating lists.py

---
+++ lists.py
@@ -0,0 +1,22 @@
+
+mylist = [1,2,3,4,5]
+
+print(mylist)
+print(mylist[0])
+
+# print hte last element in a list
+print(mylist[-1])
+
+
+print(mylist[1:3])
+
+## reverse a kust
+print(mylist[:: -1])
+
+mylist.append(6)
+print(mylist)
+
+mylist.insert(0,0)
+print(mylist)
+
+print(mylist.index(4))
